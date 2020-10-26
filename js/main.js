0384c0d27f1e36ef808886a4a53fb11069673635
unknown <vpupkin@company.com>
2020-10-26 13:22:19 +0200

    Add files for excercise "Timer"

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  minutes: document.querySelector('span[data-value="mins"]'),
+  seconds: document.querySelector('span[data-value="secs"]'),
+}
+
+class CountdownTimer {
+  constructor({ onTick, targetDate }) {
+    this.onTick = onTick;
+    this.targetDate = targetDate;
+  }
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate- currentTime;
+      const time = this.getTimeComponents(deltaTime)
+
+      this.onTick(time);
+      }, 1000)
+  };
+
+getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  
+  return { days, hours, mins, secs };
+};
+
+pad(value) { return String(value).padStart(2, '0'); };
+}
+
+const timer = new CountdownTimer({
+  onTick: updateClockFace,
+  targetDate: new Date('Jul 17, 2019'),
+});
+
+timer.start(timer)
+
+
+function updateClockFace({ days, hours, mins, secs }) { 
+  refs.days.textContent = days;
+  refs.hours.textContent = hours; 
+  refs.minutes.textContent = mins; 
+  refs.seconds.textContent = secs;
+}
